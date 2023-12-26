862ccf72d51f41fb771a6af579e5e284263528e0
Nathan Chu <nathanchu05@gmail.com>
2023-12-25 17:08:45 -0800

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projects from './pages/Projects';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
